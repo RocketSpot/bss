dba74fdf431189b42c8b9a7b5ca64f372167b6ba
Adam Ahmed <75999418+RocketSpot@users.noreply.github.com>
2024-09-18 18:50:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8592,7 +8592,7 @@ function BeeSwarmSimulator(DATA){
             
             canUseOnSlot:(slot)=>{
                 
-                return slot.type!=='basic'
+                return slot.type!=='mythic'
             },
             amount:0,u:128*4/2048,v:128*5/2048,value:60,
             use:function(){
@@ -29883,7 +29883,7 @@ function BeeSwarmSimulator(DATA){
 
     } else {
 
-        items.mythicEgg.amount=50
+        items.basicEgg.amount=50
         player.updateInventory()
 
         let tut=['Welcome to Bee Swarm Simulator!',5000,'Click the egg icon on the bar on','the left to open your inventory!',4000,'Click on the Basic Egg to select it!',4000,'Hover your mouse over a hive slot','and click to hatch it!',4000,'Collect pollen in fields with your bee!',4000,'After filling up your bag, stand near','your hive to convert the pollen into honey!',4000,'Use honey to buy more bee eggs and new tools!',5000,'Talk to bears and complete their quests!',3000,'Have fun!'],t=1000
